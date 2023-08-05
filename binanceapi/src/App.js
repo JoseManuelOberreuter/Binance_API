f18b81ef274ceda4d710fb99ae8e2cfb1c490d39
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-08-05 18:48:09 -0400

    update

---
+++ binanceapi/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {ChartComponent, Table, ApiTable} from "./Components"
+
+function App() {
+  return (
+    <div className="App">
+      <ChartComponent />
+      <Table />
+      <ApiTable/>
+
+
+    </div>
+  );
+}
+
+export default App;
